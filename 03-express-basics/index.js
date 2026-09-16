d58b070c1746ffb5987e8fa47c417e2b52f229cf
marcdev <marcellkurniawan5@gmail.com>
2026-09-16 19:48:31 +0700

    feat: express basics - routing, middleware ordering, centralized error handling

---
+++ 03-express-basics/index.js
@@ -0,0 +1,37 @@
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+
+
+const myLogger = function (req, res, next) {
+    console.log(`${req.method} ${req.path}`);
+    next();
+}
+
+app.use(myLogger);
+
+
+app.get('/', (req, res) => {
+    res.send(`Hello from Express!`);
+})
+
+app.get('/broken', (req, res) => {
+    throw new Error('Something went wrong!');
+});
+
+app.get('/greet/:name', (req, res) => {
+    res.send(`Hello ${req.params.name}`);
+})
+
+app.use((err, req, res, next) => {
+    console.error(err.message);
+    res.status(500).json({ error: err.message });
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+})
+
+
